ce3917203668e9844a916a35eb9d6d018232a79c
Andrea Rinaldi <andrea.rinaldi@euris.it>
2021-04-13 09:30:49 +0200

    fix: removed axios workaround because useless with the correct configuration of jest

--- app/src/application/smsService.js
+++ app/src/application/smsService.js
@@ -14,17 +14,6 @@ module.exports.sendSms = async (phone) => {
   const body = { from: fromField, to: phone, message: msg }
   const headers = { 'Content-Type': 'application/json', 'x-api-key': apiKey }
 
-  // *** Work Around for Network Error ***
-  // We need to change the default axios adapter,
-  // reference link:
-  // https://github.com/axios/axios/issues/938
-  // https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios
-
-  const path = require('path')
-  const lib = path.join(path.dirname(require.resolve('axios')), 'lib/adapters/http')
-  const http = require(lib)
-  axios.defaults.adapter = http
-
   let result = null
 
   await axios.post(url, body, { headers: headers })
