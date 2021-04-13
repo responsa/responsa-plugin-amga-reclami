0496f1ac8908042caf488ef3919a5da897c82d5b
Andrea Rinaldi <andrea.rinaldi@euris.it>
2021-04-13 16:03:30 +0200

    fix: renamed project name in status route

--- app/src/routes/status/statusRoute.js
+++ app/src/routes/status/statusRoute.js
@@ -3,6 +3,6 @@ module.exports = async function (fastify) {
     const ciCommit = 'CI_PUTS_HERE_LAST_GIT_COMMIT'
     const lastDeploy = 'CI_PUTS_HERE_DEPLOY_DATE'
 
-    return `ok! Plugin Demo released on ${lastDeploy}, last commit was "${ciCommit}"`
+    return `ok! Plugin Amga Reclami released on ${lastDeploy}, last commit was "${ciCommit}"`
   })
 }
