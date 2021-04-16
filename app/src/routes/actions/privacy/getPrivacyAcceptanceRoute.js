88efbfa8cd718ae8d28eb2b1158e463d2bbd3317
iacobellis <sergio.iacobellis@euris.it>
2021-04-16 09:17:15 +0200

    Add check for not found

--- app/src/routes/actions/privacy/getPrivacyAcceptanceRoute.js
+++ app/src/routes/actions/privacy/getPrivacyAcceptanceRoute.js
@@ -28,12 +28,13 @@ module.exports = async function (fastify) {
   fastify.get('/', { schema: routeSchema }, async (req, reply) => {
     const result = await zohoService.getData(`/report/Privacy_Report?criteria=(Cliente_email=="${req.query.email}")`)
     let found = false
-
     if (result.data.code === 3000) {
       found = result.data.data.sort((a, b) => b.Modified_Time - a.Modified_Time)[0].Consenso === 'SI'
       reply.code(200).send({
         result: found
       })
+    } else if (result.data.code === 3100) {
+      return found
     } else {
       reply.code(400)
     }
