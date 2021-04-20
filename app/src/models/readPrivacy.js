7ef43a570b40f899edcc4d7e98e8b4b9cc0976f5
iacobellis <sergio.iacobellis@euris.it>
2021-04-20 11:36:20 +0200

    Fix: add reference from generic errors

--- app/src/models/readPrivacy.js
+++ app/src/models/readPrivacy.js
@@ -1,3 +1,5 @@
+const genericErrors = require('./genericErrors')
+
 module.exports.readPrivacy = {
   type: 'object',
   properties: {
@@ -15,21 +17,14 @@ module.exports.readPrivacy200 = {
   $ref: 'readPrivacy#'
 }
 
-module.exports.readPrivacy400 = {
-  type: 'object',
-  description: 'Bad request',
-  $ref: 'Error#'
-}
-
-module.exports.readPrivacy500 = {
-  type: 'object',
-  description: 'Internal error',
-  $ref: 'Error#'
-}
+module.exports.readPrivacy400 = genericErrors.generic400
+module.exports.readPrivacy404 = genericErrors.generic404('Read privacy not found')
+module.exports.readPrivacy500 = genericErrors.generic500
 
 module.exports.addSchemas = (fastifyInstance) => {
   fastifyInstance.addSchema({ $id: 'readPrivacy', ...this.readPrivacy })
   fastifyInstance.addSchema({ $id: 'readPrivacy200', ...this.readPrivacy200 })
   fastifyInstance.addSchema({ $id: 'readPrivacy400', ...this.readPrivacy400 })
+  fastifyInstance.addSchema({ $id: 'readPrivacy404', ...this.readPrivacy404 })
   fastifyInstance.addSchema({ $id: 'readPrivacy500', ...this.readPrivacy500 })
 }
