26b1082eb1266a0538bb22710eb1241e0bb98735
iacobellis <sergio.iacobellis@euris.it>
2021-05-31 13:02:53 +0200

    restore error handler

--- app/src/application/errorHandler.js
+++ app/src/application/errorHandler.js
@@ -8,24 +8,13 @@ module.exports = (error, request, reply) => {
       message: error.message,
       stackTrace: error.stack
     })
-  } else if (typeof error === 'function') {
-    const errorContent = error()
-    return reply.code(errorContent.statusCode).type('application/json').send({
-      statusCode: errorContent.statusCode,
-      error: errorContent.code,
-      message: errorContent.message
-    })
-  } else if (typeof error === 'string') {
-    return reply.code(520).type('application/json').send({
-      statusCode: 520,
-      error: error,
-      message: error
-    })
-  } else {
-    return reply.code(520).type('application/json').send({
-      statusCode: 520,
-      error: 'Unknown',
-      message: `Unknown error. Error type is ${typeof error}`
-    })
   }
+
+  const errorContent = error()
+
+  return reply.code(errorContent.statusCode).type('application/json').send({
+    statusCode: errorContent.statusCode,
+    error: errorContent.code,
+    message: errorContent.message
+  })
 }
